1ffd61d817a07299446d0f2171fd560e7052209d
lzc <Lzc654321>
2021-12-10 15:12:55 +0800

    初始化modal

---
+++ src/packages/modal/modal.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import 'reflect-metadata';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+    name: 'GlModal'
+})
+export default class Modal extends Vue {
+
+}
+</script>
+
+<template src="./modal.html"></template>
+<style src="./modal.less" lang="less" scoped></style>
